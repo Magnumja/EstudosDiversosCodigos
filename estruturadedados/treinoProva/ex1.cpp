e9fcca5dffcea984fcfc8c315a6008c569006299
Magnumja <162545876+Magnumja@users.noreply.github.com>
2026-04-18 21:41:37 -0400

    up

--- estruturadedados/treinoProva/ex1.cpp
+++ estruturadedados/treinoProva/ex1.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-
+#include <cstdlib> // Necessário para a função system()
 
 using namespace std;
-
-
-// funcao antes da main
 
 void trocaValores(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-
 }
-
 
 int main(){
     int a = 5;
     int b = 10;
 
-
     cout << "Antes da troca de valores: a = " << a << ", b = " << b << endl;
-
 
     trocaValores(&a, &b);
 
-    cout << "Depois da troca de valores a = " << a << ", b = " << b << endl;
+    cout << "Depois da troca de valores: a = " << a << ", b = " << b << endl;
+
+    // Esta linha manterá o terminal aberto para você ler o resultado
+    system("pause"); 
 
     return 0;
-
 }
