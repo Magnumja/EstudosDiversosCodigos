9d5c7caef8a00a5be2aa9cf8768426fd9f970096
Magnum de Abreu <162545876+Magnumja@users.noreply.github.com>
2026-04-19 22:31:48 -0400

    Fix syntax errors and improve stack operations

--- estruturadedados/revisaop1/ex1.cpp
+++ estruturadedados/revisaop1/ex1.cpp
@@ -47,9 +47,75 @@ Sim, ainda haveria clientes. A ordem na fila para atendimento é: 13
 
 using namespace std;
 
+#define MAX 100
+// 1. O REGISTRO: É o "molde" do dado que vamos guardar
+typedef int TIPOCHAVE;
+
+typedef struct{    
+    TIPOCHAVE chave;
+}REGISTRO;
+
+// 2, pilha a estrutura que gerencia a memoria
+
+typedef struct{
+    REGISTRO A[MAX];
+    int top; // A nossa variável mais importante! Ela diz em qual "degrau" da pilha estamos.
+}PILHA;
+
+
+//Prototipos, avisando ao complicador q essas funcoes existem
+
+void initPilha(PILHA *p);
+void exibePilha(PILHA *p);
+void pushPilha(PILHA *p);
+void popPilha(PILHA *p);
+
 int main()
 {
+    REGISTRO aux; // uma caixinha temporaria para guardar i dadi digitado pelo usuario
+    PILHA pilha; // Aqui a pilha nasce de verdade na memoria
 
-    
+    // Passamos o endereco da pilha(&) para que a funcao possa alterala de verdade
+
+    initPilha(&Pilha);
+
+    int op = -1;
+    bool resp;
+
+    while(op != 0){
+
+        cout << " Bem vindo ao HELPDESK selecione o que deseja " << endl;
+        cout << " 0 - Sair" << endl;
+        cout << " 1 - Abrir ticket e aguarde operacao (inserir)" << endl;
+        cout << " 2 - Remover Elemento (Pop)" << endl;
+        cout << " 3 - Exibir pilha" << endl;
+
+        cout << " opcao: ";
+        cin >> op;
+
+        switch(op) {
+
+            case 0: 
+                cout << "Fim do atendimento." << endl;
+                break;
+            case 1;
+                cout << "Qual seu id da empresa: ";
+                cin >> aux.chave; // guarda o valor na struct auxiliar
+
+                resp = push(&pilha, aux); //Manda o endereço da pilha e a struct com o dado
+                if(resp) cout << "Ticket gerado com sucesso!" << endl 
+                break;
+            case 2:
+                resp = pop(&pilha);
+                if(resp) cout << "Elemento inserido com sucesso!" << endl;
+                break;
+            case 3:
+                exibePilha(&pilha);
+                break;
+            default:
+                cout << "Opcao invalida" << endl;
+        }             
     return 0;
 }
+
+
